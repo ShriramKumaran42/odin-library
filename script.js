3dbf0a403c40350b7ff6f94eb72bfedc791516d5
ShriramKumaran42 <shriramkumaran142000@gmail.com>
2024-12-27 02:21:36 +0530

    displaying the books fn should be added

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-const showBtn = document.getElementById("showDialog");
-const addDialog = document.getElementById("addDialog");
 const outputBox = document.querySelector("output")
-const bookTitle = document.querySelector("#bookTitle");
-const bookAuthor = document.querySelector("#bookAuthor");
-const bookPgs = document.querySelector("#bookPgs");
-const finished = document.querySelector("select")
-const confirmBtn = document.querySelector("#confirmBtn");
+
 
 //shows the modal <dialog>
-showBtn.addEventListener("click", () => {
-    addDialog.showModal();
+const dialog = document.querySelector("dialog");
+const showBtn = document.querySelector("#showDialog");
+    showBtn.addEventListener("click", () => {
+    dialog.showModal();
 });
 
-addDialog.addEventListener("close", (e) => {
-    outputBox.value = 
-    addDialog.returnValue === "default"
-    ? "No return value"
-    : `ReturnValue: ${favDialog.returnValue}`;
-})
-
+//addBook btn
+const confirmBtn = document.querySelector("#confirmBtn");
 confirmBtn.addEventListener("click", (event) => {
+    const form = document.querySelector(form);
+
+    if(form.checkValidity()) {
+        event.preventDefault();
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const pages = document.getElementById("pages").value;
+        const read = document.getElementById("read").checked ? "Yes" : "No";
+        addBookToLibrary(title, author, pages, read);
+
+        dialog.close();
+        document.querySelector("form").reset();
+    }
+});
+
+//close
+const closeBtn = document.querySelector(".cancel");
+closeBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    addDialog.closest()    
+    dialog.close();
+    document.querySelector("form").reset();
 })
 
 
+//books
 
 const myLibrary = [];
 
@@ -33,14 +44,22 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`
-    }
 }
+
+Book.prototype.info = function() {
+    return [this.title, this.author, this.pages, this.read];
+}
+
+
 
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book (title, author, pages, read)
     myLibrary.push(newBook);
+    addBookViewer(newBook);
+}
+
+function addBookViewer(newBook){
+    
 }
 
 addBookToLibrary("Hobbit", "J.R.R TOLKEIN", 669, true);
