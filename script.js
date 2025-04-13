43abe8b87ac6a1fe523a32b39ecc4949db536a5a
ShriramKumaran42 <shriramkumaran142000@gmail.com>
2025-04-14 00:15:51 +0530

    Real Time Validation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const { isValid } = require("rsuite/esm/utils/dateUtils");
+//const { isValid } = require("rsuite/esm/utils/dateUtils");
 
 
 
@@ -7,24 +7,51 @@ const dialog = document.querySelector("dialog");
 const showBtn = document.querySelector("#showDialog");
     showBtn.addEventListener("click", () => {
     dialog.showModal();
+    // initializeValidation();
 });
 
 //addBook btn
 const confirmBtn = document.querySelector("#confirmBtn");
 confirmBtn.addEventListener("click", (event) => {
-    const form = document.querySelector("form");
+    event.preventDefault();
 
-    if(form.checkValidity()) {
-        event.preventDefault();
+    const nameValid = isValidName();
+    const authorValid = isValidAuthor();
+    const pagesValid = isValidPages();
+
+    setNameClass(nameValid);
+    updateBookError(nameValid);
+
+    setAuthorClass(authorValid);
+    updateAuthorError(authorValid);
+
+    setPagesClass(pagesValid);
+    updatePagesError(pagesValid);
+
+    if (nameValid && authorValid && pagesValid) {
         const title = document.getElementById("title").value;
         const author = document.getElementById("author").value;
         const pages = document.getElementById("pages").value;
         const read = document.getElementById("read").checked ? "Yes" : "No";
-        addBookToLibrary(title, author, pages, read);
 
+        addBookToLibrary(title, author, pages, read);
         dialog.close();
-        document.querySelector("form").reset();
+        form.reset();
     }
+
+    // const form = document.querySelector("form");
+
+    // if(form.checkValidity()) {
+    //     event.preventDefault();
+    //     const title = document.getElementById("title").value;
+    //     const author = document.getElementById("author").value;
+    //     const pages = document.getElementById("pages").value;
+    //     const read = document.getElementById("read").checked ? "Yes" : "No";
+    //     addBookToLibrary(title, author, pages, read);
+
+    //     dialog.close();
+    //     document.querySelector("form").reset();
+    // }
 });
 
 //close
@@ -68,35 +95,58 @@ function addBookToLibrary(title, author, pages, read) {
 function addBookViewer(newBook){
     const view = document.createElement("div");
     view.classList.add("view")
+
     const bookInfo = document.createElement("p");
     bookInfo.textContent = `Title of the Book: ${newBook.title} \n Author Name: ${newBook.author} \n No of Pages: ${newBook.pages} \n Have Read: ${newBook.read}`;
     view.appendChild(bookInfo);
 
-    let readToggleBtn = document.createElement("button");
-    readToggleBtn.classList.add("readToggleBtn");
+    const readToggleBtn = document.createElement("button");
     readToggleBtn.textContent = "Read or Not";
     readToggleBtn.addEventListener("click", () => {
         newBook.readChange();
         bookInfo.textContent = `Title of the Book: ${newBook.title} \n Author Name: ${newBook.author} \n No of Pages: ${newBook.pages} \n Have Read: ${newBook.read}`;
-    });
+    })
     view.appendChild(readToggleBtn);
+
+    const dltBtn = document.createElement("button");
+    dltBtn.textContent = "Delete";
+    dltBtn.addEventListener("click", () => {
+        const index = myLibrary.indexOf(newBook);
+        if (index > -1) {
+            myLibrary.splice(index, 1);
+        }
+        view.remove();
+    })
+    view.appendChild(dltBtn);
+
+    const outputBox = document.querySelector("output");
+    outputBox.appendChild(view);
+
+    // let readToggleBtn = document.createElement("button");
+    // readToggleBtn.classList.add("readToggleBtn");
+    // readToggleBtn.textContent = "Read or Not";
+    // readToggleBtn.addEventListener("click", () => {
+    //     newBook.readChange();
+    //     bookInfo.textContent = `Title of the Book: ${newBook.title} \n Author Name: ${newBook.author} \n No of Pages: ${newBook.pages} \n Have Read: ${newBook.read}`;
+    // });
+    // view.appendChild(readToggleBtn);
         
 
-        let dltBtn = document.createElement("button");
-        dltBtn.classList.add("dltBtn");
-        dltBtn.textContent = "Delete this Book";
-        dltBtn.addEventListener("click", () => {
-            const index = myLibrary.indexOf(newBook);
-            if(index > -1) {
-                myLibrary.splice(index, 1);
-            }
-            view.remove();
-        })
-        view.appendChild(dltBtn);
+        // let dltBtn = document.createElement("button");
+        // dltBtn.classList.add("dltBtn");
+        // dltBtn.textContent = "Delete this Book";
+        // dltBtn.addEventListener("click", () => {
+        //     const index = myLibrary.indexOf(newBook);
+        //     if(index > -1) {
+        //         myLibrary.splice(index, 1);
+        //     }
+        //     view.remove();
+        // })
+        // view.appendChild(dltBtn);
 
 
-        let outputBox = document.querySelector("output");
-        outputBox.appendChild(view);
+        // let outputBox = document.querySelector("output");
+        // outputBox.appendChild(view);
 }
 
 // function updateBookView(view, newBook){
@@ -119,9 +169,9 @@ function addBookViewer(newBook){
 //------------VALIDATIONS------------------//
 
 const form = document.querySelector("form");
-const bookName = document.getElementById("name Error");
-const author = document.getElementById("author Error");
-const pages = document.getElementById("pages Error");
+const bookName = document.getElementById("title");
+const authorName = document.getElementById("author");
+const pagesNo = document.getElementById("pages");
 const bookError = document.getElementById("name-error");
 const authorError = document.getElementById("author-error");
 const pagesError = document.getElementById("pages-error");
@@ -132,12 +182,12 @@ const isValidName = () => {
 }
 
 const isValidAuthor = () => {
-    const authorValidity = author.value.length !== 0;
+    const authorValidity = authorName.value.length !== 0;
     return authorValidity;
 }
 
 const isValidPages = () => {
-    const pagesValidity = pages.value.length !== 0;
+    const pagesValidity = pagesNo.value.length !== 0;
     return pagesValidity;
 }
 
@@ -146,11 +196,11 @@ const setNameClass = (isValid) => {
 }
 
 const setAuthorClass = (isValid) => {
-    author.className = isValid ? "valid" : "invalid";
+    authorName.className = isValid ? "valid" : "invalid";
 }
 
 const setPagesClass = (isValid) => {
-    pages.className = isValid ? "valid" : "invalid";
+    pagesNo.className = isValid ? "valid" : "invalid";
 }
 
 const updateBookError = (isValidName) => {
@@ -199,7 +249,7 @@ const initializeValidation = () => {
 
 const handleInput = () => {
     const nameInput = isValidName();
-    setValidClass(nameInput);
+    setNameClass(nameInput);
     updateBookError(nameInput);
 
     const authorInput = isValidAuthor();
@@ -211,29 +261,49 @@ const handleInput = () => {
     updatePagesError(pagesInput);
 }
 
-const handleSubmit = (event) => {
-    event.preventDefault();
+// const handleSubmit = (event) => {
+//     event.preventDefault();
 
-    setValidClass(nameInput);
-    updateBookError(nameInput);
+//     setNameClass(nameInput);
+//     updateBookError(nameInput);
     
-    setAuthorClass(authorInput);
-    updateAuthorError(authorInput);
+//     setAuthorClass(authorInput);
+//     updateAuthorError(authorInput);
     
-    setPagesClass(pagesInput);
-    updatePagesError(pagesInput);
+//     setPagesClass(pagesInput);
+//     updatePagesError(pagesInput);
 
+// }
+
+// showBtn.addEventListener("click", () => {
+//     initializeValidation();
+// })
+
+function handleBlur(event) {
+    const field = event.target;
+
+    if (field === bookName) {
+        const valid = isValidName();
+        setNameClass(valid);
+        updateBookError(valid);
+    }
+    else if (field === authorName) {
+        const valid = isValidAuthor();
+        setAuthorClass(valid);
+        updateAuthorError(valid);
+    }
+    else if (field === pagesNo) {
+        const valid = isValidPages();
+        setPagesClass(valid);
+        updatePagesError(valid);
+    }
 }
 
-showBtn.addEventListener("click", () => {
-    initializeValidation();
-})
+bookName.addEventListener("blur", handleInput);
+authorName.addEventListener("blur", handleInput);
+pagesNo.addEventListener("blur", handleInput);
 
-bookName.addEventListener("input", handleInput);
-author.addEventListener("input", handleInput);
-pages.addEventListener("input", handleInput);
-
-form.addEventListener("submit", handleSubmit)
+// form.addEventListener("submit", handleSubmit)
 
 
 
